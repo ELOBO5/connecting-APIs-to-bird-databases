df07fd5108419795611c0ad4d77ff1e08c40c50c
“ELOBO5” <elicialobo@hotmail.com>
2021-08-21 21:18:06 +0100

    create routes

---
+++ server-postgresql/api/controllers/birds.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+
+const Bird = require("../models/bird");
+
+// // bird index route
+router.get("/", async (req, res) => {
+  try {
+    const birds = await Bird.all;
+    res.json({ birds });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
+// // birds show route
+router.get("/:id", async (req, res) => {
+  try {
+    const bird = await Bird.findById(parseInt(req.params.id));
+    res.json(bird);
+  } catch (err) {
+    res.status(404).json({ err });
+  }
+});
+
+// Create bird route
+router.post("/", async (req, res) => {
+  try {
+    const bird = await Bird.create(req.body.name, req.body.age, req.body.breed);
+    res.json(bird);
+  } catch (err) {
+    res.status(404).json({ err });
+  }
+});
+
+// // birds update route
+router.patch("/:id", async (req, res) => {
+  try {
+    const bird = await Bird.findById(parseInt(req.params.id));
+    const updatedBird = await bird.updatedBird(
+      req.body.name,
+      req.body.age,
+      req.body.breed
+    );
+    res.json({ bird: updatedBird });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
+// // delete bird route
+router.delete("/:id", async (req, res) => {
+  try {
+    const bird = await Bird.findById(parseInt(req.params.id));
+    await bird.destroy();
+    res.status(204).json("Bird deleted");
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
+module.exports = router;
